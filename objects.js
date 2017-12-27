b70f5ca9af4629d6989065884c240e741dc12b38
Jon Osterman <suni.abraham@gmail.com>
2017-12-27 13:56:54 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,12 @@
 var playlist = {
-  'kanye': 'golddigger',
-  'kidkudi': 'dayandnight'
-  'pushat': 'grindn'
+  'kanye': 'golddigger'
 };
 
 function updatePlaylist(obj,key,value) {
-  return Object.assign(obj, {[key]:value})
+  return Object.assign({},obj, {[key]:value})
 }
 
-function removeFromPlaylist(obj2,key2)  {
+function removeFromPlaylist(plylist,key2)  {
   delete obj2.key2;
 
   return playlist;
